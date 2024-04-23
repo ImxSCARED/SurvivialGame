1bcced069a0f1d394efa305663d718adbc0b6cf3
IMxSCARED <brianmcnamee7503@gmail.com>
2024-04-23 15:58:58 +1000

    Survival game, inventory and wokring health,thirst,hunger bars. base

---
+++ Survial Game/Survival game/Assets/Scripts/Inventory/Slot.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public ItemSO data;
+    public int stackSize;
+    [Space]
+    public Image icon;
+    public Text stackText;
+
+
+    private bool isEmpty;
+    public bool IsEmpty => isEmpty;
+
+
+    private void Start()
+    {
+        UpdateSlot();
+    }
+
+
+    public void UpdateSlot()
+    {
+        if (data == null)
+        {
+            isEmpty = true;
+
+            icon.gameObject.SetActive(false);
+            stackText.gameObject.SetActive(false);
+        }
+        else
+        {
+            isEmpty = false;
+
+            icon.sprite = data.icon;
+            stackText.text = $"x{stackSize}";
+
+            icon.gameObject.SetActive(true);
+            stackText.gameObject.SetActive(true);
+        }
+    }
+
+        public void AddItemToSlot(ItemSO data_, int stackSize_)
+        {
+            data = data_;
+            stackSize = stackSize_;
+        }
+    public void AddStackAmount(int stackSize_)
+    {
+     
+        stackSize += stackSize_;
+    }
+
+
+    public void Drop()
+    {
+       GetComponentInParent<Inventorymanager>().DropItem(this);
+    }
+
+    public void Clean()
+    {
+        data = null;
+        stackSize = 0;
+
+        UpdateSlot();
+    }
+}
